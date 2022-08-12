469f2f79f760573ab7f44142ca40cec9d13dc348
amirhd81 <amir.hd.davoudi1381@gmail.com>
2022-08-12 20:14:27 +0430

    adding the api call to receive weather data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { Navigation } from "./Navigation";
 import { store } from "./redux/config";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Provider store={store}>
-        <div></div>
+        <Navigation />
       </Provider>
     </BrowserRouter>
   );
--- src/containers/SearchWeatherForecast/SearchWeatherForecast.tsx
+++ src/containers/SearchWeatherForecast/SearchWeatherForecast.tsx
@@ -16,6 +16,37 @@ export const SearchWeatherForecast = () => {
   const [selectedCity, setSelectedCity] =
     useState<SingleValue<CityType> | null>(null);
 
+  const [groupedWeatherData, setGroupedWeatherData] = useState<any[]>([]);
+
+  const splitDate = (date: string) => {
+    return date.split(" ")[0];
+  };
+
+  const onGroupWeatherData = (dataList: any[]) => {
+    const groupedData: any[] = [];
+    let dataDailyBased: any[] = [];
+
+    dataList.forEach((data) => {
+      const dataDate = splitDate(data.dt_txt);
+
+      if (!dataDailyBased.length) {
+        return dataDailyBased.push(data);
+      }
+
+      if (
+        splitDate(dataDailyBased[dataDailyBased.length - 1].dt_txt) === dataDate
+      ) {
+        return dataDailyBased.push(data);
+      }
+
+      groupedData.push(dataDailyBased);
+      dataDailyBased = [];
+      dataDailyBased.push(data);
+    });
+
+    setGroupedWeatherData(groupedData);
+  };
+
   const onChangeCity = (selectedCity?: SingleValue<CityType>) => {
     if (!selectedCity) return;
 
@@ -25,6 +56,16 @@ export const SearchWeatherForecast = () => {
   useEffect(() => {
     if (!selectedCity) return;
 
+    const AxiosInstance = axios.create({
+      baseURL: "https://api.openweathermap.org/data/2.5",
+    });
+    https: AxiosInstance.get(
+      `/forecast?lat=${selectedCity.lat}&lon=${selectedCity.lng}&appid=5902d95571c554f2994f0d48b25169ee&units=metric`
+    )
+      .then((res) => {
+        onGroupWeatherData(res.data.list);
+      })
+      .catch((err) => console.log(err));
   }, [selectedCity]);
 
   return (
